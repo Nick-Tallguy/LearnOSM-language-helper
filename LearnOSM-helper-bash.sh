9f3d4ba120bce2bb757a5dc8997c2373669f65b7
Nick-Tallguy <nick.allen.54@gmail.com>
2017-04-24 18:25:49 +0100

    interim files production added - beginner and coordination

--- LearnOSM-helper-bash.sh
+++ LearnOSM-helper-bash.sh
@@ -21,3 +21,20 @@ sed -f $script_path/sed_LearnOSM_index.script $trans_path/0100-01-01-index.md $t
 sed -i s/zzxz/${lang}/ $output_path/0100-01-01-index_$lang.md
 sed -f $script_path/sed_LearnOSM_status.script $trans_path/0100-01-01-status.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt>$output_path/0100-01-01-status_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0100-01-01-status_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_introduction.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0200-12-29-introduction_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0200-12-29-introduction_interim_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_start-osm.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0200-12-27-start-osm_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0200-12-27-start-osm_interim_$lang.md
+sed -i s/introduction/start-osm/ $output_path/0200-12-27-start-osm_interim_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_id-editor.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0200-12-23-id-editor_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0200-12-23-id-editor_interim_$lang.md
+sed -i s/introduction/id-editor/ $output_path/0200-12-23-id-editor_interim_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_glossary.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0200-12-05-glossary_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0200-12-05-glossary_interim_$lang.md
+sed -i s/introduction/glossary/ $output_path/0200-12-05-glossary_interim_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_moving-forward.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0200-12-03-moving-forward_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0200-12-03-moving-forward_interim_$lang.md
+sed -i s/introduction/moving-forward/ $output_path/0200-12-03-moving-forward_interim_$lang.md
+sed -f $script_path/sed_LearnOSM_interim_humanitarian.script $trans_path/interim_en.md $trans_path/for_use_learnosm-1_essential_setuptxt_$lang.txt $trans_path/languages.txt>$output_path/0500-10-27-humanitarian_interim_$lang.md
+sed -i s/zzxz/${lang}/ $output_path/0500-10-27-humanitarian_interim_$lang.md
+sed -i s_/beginner/introduction_/coordination/humanitarian_ $output_path/0500-10-27-humanitarian_interim_$lang.md
---
+++ scripts/from_transifex/interim_en.md
@@ -0,0 +1,16 @@
+---
+layout: doc
+title: JOSM adding tms, wms or wmts imagery
+permalink: /en/josm/josm-adding-imagery/
+lang: en
+category: josm
+published: true
+---
+
+JOSM adding tms, wms or wmts imagery
+=============================  
+
+This guide is currently unavailable. It is being translated. Please refer to it in   
+aaabbbccc
+To help produce translations for this site, go to [https://www.transifex.com](https://www.transifex.com) and search for **HOT-OSM**.
+
