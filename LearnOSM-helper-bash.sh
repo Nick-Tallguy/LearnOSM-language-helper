333aa0cbdf52eb65fbd6e32af14d2674312f1fdd
Nick-Tallguy <nick.allen.54@gmail.com>
2017-06-09 16:33:26 +0100

    amend script to avoid clash of characters in language codes

--- LearnOSM-helper-bash.sh
+++ LearnOSM-helper-bash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #NEEDS TO BE SET for each new language - language for file production - for some languages it will be more than 2 characters, such as "zh-tw".
-lang=sq
+lang=nl_NL
 #
 #location of sed scripts
 script_path='/home/nick/Github/LearnOSM-language-helper/script-and-source-files'
@@ -27,40 +27,40 @@ sed -i s/zzxz/${lang}/ $output_path/0100-01-01-status_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_introduction.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0200-12-29-introduction_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0200-12-29-introduction_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0200-12-29-introduction_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0200-12-29-introduction_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_start-osm.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0200-12-27-start-osm_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0200-12-27-start-osm_interim_$lang.md
 sed -i s/introduction/start-osm/ $output_path/0200-12-27-start-osm_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0200-12-27-start-osm_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0200-12-27-start-osm_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_id-editor.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0200-12-23-id-editor_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0200-12-23-id-editor_interim_$lang.md
 sed -i s/introduction/id-editor/ $output_path/0200-12-23-id-editor_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0200-12-23-id-editor_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0200-12-23-id-editor_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_glossary.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0200-12-05-glossary_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0200-12-05-glossary_interim_$lang.md
 sed -i s/introduction/glossary/ $output_path/0200-12-05-glossary_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0200-12-05-glossary_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0200-12-05-glossary_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_moving-forward.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0200-12-03-moving-forward_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0200-12-03-moving-forward_interim_$lang.md
 sed -i s/introduction/moving-forward/ $output_path/0200-12-03-moving-forward_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0200-12-03-moving-forward_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0200-12-03-moving-forward_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_humanitarian.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0500-10-27-humanitarian_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0500-10-27-humanitarian_interim_$lang.md
 sed -i s_/beginner/introduction_/coordination/humanitarian_ $output_path/0500-10-27-humanitarian_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0500-10-27-humanitarian_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0500-10-27-humanitarian_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_coordination.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0500-12-31-coordination_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0500-12-31-coordination_interim_$lang.md
 sed -i s_/beginner/introduction_/coordination_ $output_path/0500-12-31-coordination_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0500-12-31-coordination_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0500-12-31-coordination_interim_$lang.md
 #
 sed -f $script_path/sed_LearnOSM_interim_remote-tracing.script $script_path/interim_en.md $trans_path/essential_setup_$lang.txt $script_path/languages.txt>$output_path/0500-10-15-remote-tracing_interim_$lang.md
 sed -i s/zzxz/${lang}/ $output_path/0500-10-15-remote-tracing_interim_$lang.md
 sed -i s_/beginner/introduction_/coordination_ $output_path/0500-10-15-remote-tracing_interim_$lang.md
-sed -i s_glish]\(\/${lang}_glish]\(\/en_ $output_path/0500-10-15-remote-tracing_interim_$lang.md
+sed -i s^glish]\(\/${lang}^glish]\(\/en^ $output_path/0500-10-15-remote-tracing_interim_$lang.md
 #
